Nickdv9/RR_build

rr: optional root methods [2/2]
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

tools/releasetools/ota_from_target_files.py
@@ -823,14 +823,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
-  if block_based:
-    script.Print(" ")
-    script.Print("Flashing Magisk...")
-    script.Print(" ")
-    common.ZipWriteStr(output_zip, "magisk/magisk.zip",
-                   ""+input_zip.read("SYSTEM/addon.d/magisk.zip"))
-    script.FlashMagisk()
-    script.Print(" ")
+  if os.getenv('ROOT_METHOD','rootless') == "magisk":
+    if block_based:
+      script.Print(" ")
+      script.Print("Flashing Magisk...")
+      script.Print(" ")
+      common.ZipWriteStr(output_zip, "magisk/magisk.zip",
+                     ""+input_zip.read("SYSTEM/addon.d/magisk.zip"))
+      script.FlashMagisk()
+      script.Print(" ")
+
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
 

geeksbygirls/android_build

  * add WITH_ROOT_METHOD to dumpvar
Conflicts:
	tools/releasetools/ota_from_target_files.py

tools/releasetools/ota_from_target_files.py
@@ -865,6 +865,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
+  if os.getenv('WITH_ROOT_METHOD','rootless') == "magisk":
+    if block_based:
+      script.Print(" ")
+      script.Print("Flashing Magisk...")
+      script.Print(" ")
+      common.ZipWriteStr(output_zip, "magisk/magisk.zip",
+                     ""+input_zip.read("SYSTEM/addon.d/magisk.zip"))
+      script.FlashMagisk()
+      script.Print(" ")
+
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
 