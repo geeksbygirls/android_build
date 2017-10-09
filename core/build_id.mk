unlegacy-devices/android_build-1

Merge tag 'android-7.1.2_r2' into cm-14.1
Android 7.1.2 Release 2 (N2G47E)

Change-Id: Ie3d3e9ea5c8dd67f44e43b2ed5a97e844c19888d

core/build_id.mk
@@ -18,4 +18,4 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=NOF27B
+export BUILD_ID=N2G47E

geeksbygirls/android_build

Conflicts:
	core/build_id.mk
	core/version_defaults.mk

core/build_id.mk
@@ -18,4 +18,4 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=N2G47J
+export BUILD_ID=NJH47B